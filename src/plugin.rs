363e2487a3caadd63b0cb6f6c74c8df724e4ad6e
__SyS__ <sreyassreelal@gmail.com>
2018-11-03 11:42:39 +0530

    added documentation

--- src/plugin.rs
+++ src/plugin.rs
@@ -8,58 +8,88 @@ define_native!(create,path_to_script:String);
 define_native!(response,bot:usize,query:String,string:ref Cell,size:usize);
 
 pub struct ChatBot{
-    bots:Vec<Eliza>,
+	bots:Vec<Eliza>,
 }
+
 impl ChatBot {
-    pub fn load(&self) -> bool {
-        log!("[ChatBotPlugin] Plugin started");
-        return true;
-    }
+	pub fn load(&self) -> bool {
+		log!("[ChatBotPlugin] Plugin started");
+		return true;
+	}
 
-    pub fn unload(&self) {
-         log!("[ChatBotPlugin] Plugin unloaded");
-    }
+	pub fn unload(&self) {
+		 log!("[ChatBotPlugin] Plugin unloaded");
+	}
 
-    pub fn amx_load(&mut self, amx: &mut AMX) -> Cell {
-        let natives = natives!{
-            "ChatBotCreate" => create,
-            "BotResponse" => response
-        };
-        match amx.register(&natives) {
-            Ok(_) => log!("[ChatBotPlugin] Natives are successful loaded"),
-            Err(err) => log!("[ChatBotgitPlugin] Failed to register amx instance {:?}", err),
-        }
+	pub fn amx_load(&mut self, amx: &mut AMX) -> Cell {
+		let natives = natives!{
+			"ChatBotCreate" => create,
+			"BotResponse" => response
+		};
+		match amx.register(&natives) {
+			Ok(_) => log!("[ChatBotPlugin] Natives are successful loaded"),
+			Err(err) => log!("[ChatBotgitPlugin] Failed to register amx instance {:?}", err),
+		}
 
-        AMX_ERR_NONE
-    }
+		AMX_ERR_NONE
+	}
 
-    pub fn amx_unload(&self, _amx: &AMX) -> Cell {
-        AMX_ERR_NONE
-    }
-    
-    pub fn create(&mut self,_:&AMX,path_to_script:String) -> AmxResult<Cell>{
-        let full_path = "scripts/".to_owned() + path_to_script.as_str();
-        match Eliza::new(full_path.as_str()) {
-            Ok(bot) => {
-                self.bots.push(bot);
-                Ok(self.bots.len() as Cell - 1)
-            },
-            Err(_) => Ok(-1),
-        }
-    }
-    pub fn response(&mut self,_:&AMX,bot:usize,query:String, string:&mut Cell,size:usize) -> AmxResult<Cell>{
-        let encoded = samp_sdk::cp1251::encode(self.bots[bot].respond(query.as_str()).as_str())?;
-        set_string!(encoded,string,size);
-        Ok(1)
-    }
+	pub fn amx_unload(&self, _amx: &AMX) -> Cell {
+		AMX_ERR_NONE
+	}
+	
+	/*
+		Native: ChatBotCreate
+		
+		Description:
+			Creates an eliza bot instance and push the instance to global vector
+		
+		Parameters:
+			- `path[]`: string containing path to bot script file
+		
+		Returns:
+			- `0 .. etc`: Pushed instance' index
+			- `-1`: If failed to create an instance
 
-    
+	*/
+	pub fn create(&mut self,_:&AMX,path_to_script:String) -> AmxResult<Cell>{
+		let full_path = "scripts/".to_owned() + path_to_script.as_str();
+		match Eliza::new(full_path.as_str()) {
+			Ok(bot) => {
+				self.bots.push(bot);
+				Ok(self.bots.len() as Cell - 1)
+			},
+			Err(_) => Ok(-1),
+		}
+	}
+
+	/*
+		Native: BotResponse
+		
+		Description:
+			Generates a response from the query send to the bot
+		
+		Parameters:
+			- `ChatBot:bot`: Bot instance id
+			- `query[]`: Message to send to user
+			- `string[]`: String to store response from bot
+			- `size`: size of the destination string
+		
+		Returns:`-1`
+
+	*/
+	pub fn response(&mut self,_:&AMX,bot:usize,query:String, string:&mut Cell,size:usize) -> AmxResult<Cell>{
+		let encoded = samp_sdk::cp1251::encode(self.bots[bot].respond(query.as_str()).as_str())?;
+		set_string!(encoded,string,size);
+		Ok(1)
+	}
+
 }
 
 impl Default for ChatBot {
-    fn default() -> Self {
-        ChatBot{
-            bots: Vec::new(),
-        }
-    }
+	fn default() -> Self {
+		ChatBot{
+			bots: Vec::new(),
+		}
+	}
 }
